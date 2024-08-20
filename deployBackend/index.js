cdfe25e517bbbc19581afcc957a69b12db2a6048
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-08-20 23:34:04 +0530

    Update PORT

--- deployBackend/index.js
+++ deployBackend/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
-
+const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -12,6 +12,6 @@ app.get('/signup', (req, res) => {
   res.send('<h2>Signup to explore us!!</h2>')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`)
 })
